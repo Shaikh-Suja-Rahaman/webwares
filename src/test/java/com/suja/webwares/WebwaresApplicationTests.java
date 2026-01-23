8a9bd497fe767ad90e582b05478dab8833344cef
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2026-01-23 15:36:45 +0530

    initial commit

---
+++ src/test/java/com/suja/webwares/WebwaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suja.webwares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebwaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
